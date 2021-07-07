51288ae8aac342f9486d3c5605806c8ec3091f71
Aditya Kumar Jha <mail@adijha.com>
2021-07-07 12:00:56 +0545

    it ran

--- app/src/main/java/com/adijha/externalexam/MainActivity.java
+++ app/src/main/java/com/adijha/externalexam/MainActivity.java
@@ -1,74 +1,72 @@
 package com.adijha.externalexam;
 
-//import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.*;
-import android.view.View.OnClickListener;
-import android.widget.*;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends Activity implements OnClickListener,TextWatcher{
+public class MainActivity extends AppCompatActivity {
 
-    EditText name;
-    EditText pass;
-    Button login;
-    Button cancel;
-
+    private static final int MAKE_CALL_PERMISSION_REQUEST_CODE = 1;
+    private Button dial;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        name=(EditText )findViewById(R.id.editText1);
-        name.addTextChangedListener(this);
-        pass=(EditText )findViewById(R.id.editText2);
-        pass.addTextChangedListener(this);
-        login=(Button )findViewById(R.id.button1);
-        cancel=(Button )findViewById(R.id.button2);
-        login.setOnClickListener(this);
-        cancel.setOnClickListener(this);
-        login.setEnabled(false);
-        cancel.setEnabled(true);
+        dial = (Button) findViewById(R.id.dial);
+        final EditText numberToDial = (EditText) findViewById(R.id.number);
+
+        dial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phoneNumber = numberToDial.getText().toString();
+
+                if (!TextUtils.isEmpty(phoneNumber)) {
+                    if (checkPermission(Manifest.permission.CALL_PHONE)) {
+                        String dial = "tel:" + phoneNumber;
+                        startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(dial)));
+                    } else {
+                        Toast.makeText(MainActivity.this, "Permission Call Phone denied", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "Enter a phone number", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        if (checkPermission(Manifest.permission.CALL_PHONE)) {
+            dial.setEnabled(true);
+        } else {
+            dial.setEnabled(false);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, MAKE_CALL_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    private boolean checkPermission(String permission) {
+        return ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED;
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
-
-    @SuppressLint("ShowToast") @Override
-    public void onClick(View v) {
-
-    }
-
-    public void afterTextChanged(Editable arg0) {
-
-
-    }
-
-    public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,int arg3) {
-
-
-    }
-
-    public void onTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
-
-        String na=name.getText().toString();
-        String pa=pass.getText().toString();
-
-        if(na.equals("chaitu") && pa.equals("root"))
-        {
-            Button btn = (Button) findViewById(R.id.button1);
-            btn.setEnabled(true)
-
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode) {
+            case MAKE_CALL_PERMISSION_REQUEST_CODE :
+                if (grantResults.length > 0 && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    dial.setEnabled(true);
+                    Toast.makeText(this, "You can call the number by clicking on the button", Toast.LENGTH_SHORT).show();
+                }
+                return;
         }
-
     }
-
 }
